80b637fc56b96b841f48008fc682f5fa9c7b6caf
zhiavolnov <152493447+zhiavolnov@users.noreply.github.com>
2024-04-21 01:37:20 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,13 @@
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+    }
+            System.out.println("Random Number: " + item);
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+}
+
+        for (int item : data) {
+    public static void main(String[] args) {
+        }
